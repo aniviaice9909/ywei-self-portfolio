1dd1016fd79dbfd76de0b5b05771bb700d22cb28
yewwei9909 <yewwei9909@hotmail.com>
2023-05-11 03:47:52 +0800

    changed background color version 18

--- setup.sh
+++ setup.sh
@@ -6,17 +6,15 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-\n\
 [theme]\n\
 primarycolor = "#C29AF5"\n\
 backgroundColor = "#003C7F"\n\
 secondaryBackgroundColor = "#040404"\n\
 textColor = "#FAFAFA"\n\
-\n\
 [params.background]\n\
 color = "#003C7F"\n\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
